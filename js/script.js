9227dad91724760fc53b3cce655d9ed2cdd9fb31
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2022-07-29 18:52:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import config from "./config.js";
-const API_Key = config.API_KEY;
+const API_Key = config;
 
 function show(shown, hidden) { // Function used to hide or show the second window with the art info
   document.getElementById(shown).style.display = "block";

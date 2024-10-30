6ef992717d97cde981df1429e0708563888b9383
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2024-10-29 21:47:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const input = document.querySelector(".input__search");
 const fetchArt = async () => {
   
   const APIResponse = await fetch(
-    `https://api.nookipedia.com/nh/art?api_key=0a283467-aec0-4c49-ad32-72b98ae5cfe4`
+    `https://api.nookipedia.com/nh/art?api_key=${API_Key}`
   );
 
   const data = await APIResponse.json();
